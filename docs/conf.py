54f85d8bb793d0d02b84027449c299b180b2a6fd
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-03-29 17:34:03 +0100

    modify conf.py so that sphinx automatically looks for docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,56 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+import sphinx_rtd_theme
+
+# -- Path setup --------------------------------------------------------------
+sys.path.insert(0, Path("../..").resolve())
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Pattern Discovery Kit'
-copyright = '2025, Mansour Benbakoura'
-author = 'Mansour Benbakoura'
-release = '0.2.3'
+project = "Pattern Discovery Kit"
+copyright = "2025, Mansour Benbakoura"
+author = "Mansour Benbakoura"
+release = "0.2.3"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "sphinx.ext.autodoc",  # Pulls docstrings
+    "sphinx.ext.autosummary",  # Generates summaries
+    "sphinx.ext.napoleon",  # Supports Google/Numpy docstrings
+    "sphinx.ext.viewcode",  # Links to source code
+]
 
-extensions = []
+autosummary_generate = True  # Automatically generate summaries
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# -- Autodoc settings --------------------------------------------------------
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+autodoc_typehints = "description"  # Show type hints in descriptions
+
+# -- Napoleon settings (for Google/Numpy docstrings) -------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+
+# -- HTML output -------------------------------------------------------------
+html_theme = "sphinx_rtd_theme"
 
 
+# -- Auto-generate API docs --------------------------------------------------
+def run_apidoc(_):
+    """Run sphinx-apidoc to auto-generate .rst files"""
+    module_path = os.path.abspath("../../my_package")  # Adjust path
+    output_path = os.path.abspath("./source")
+    subprocess.run(
+        ["sphinx-apidoc", "-o", output_path, module_path, "--force"], check=True
+    )
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+def setup(app):
+    app.connect("builder-inited", run_apidoc)
