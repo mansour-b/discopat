0324600a3988ba953583c02c187e2648e9f0f468
Mansour Benbakoura <52276841+mansour-b@users.noreply.github.com>
2025-04-10 07:24:19 +0200

    feat: add necessary files for readthedocs (#12)
    * configure python install in readthedocs.yaml
    
    * update gitignore
    
    * first version of the doc
    
    * modify conf.py so that sphinx automatically looks for docstrings
    
    * add sphinx rtd scheme to doc requirements
    
    * add sphinx_rtd_theme to extensions
    
    * specify package path in conf.py
    
    * Rewrite index introduction
    
    * ignore autogenerated rst files
    
    * add autosummary in index
    
    * add modules to toc + ignore modules.rst in git
    
    * remove autogenerated files
    
    * ignore everything in docs/source
    
    ---------
    
    Co-authored-by: Mansour Benbakoura <mansour.benbakoura@inria.fr>

---
+++ docs/conf.py
@@ -0,0 +1,54 @@
+import subprocess
+import sys
+from pathlib import Path
+
+# -- Path setup --------------------------------------------------------------
+sys.path.insert(0, Path("../patrick").resolve())
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Pattern Discovery Kit"
+copyright = "2025, Mansour Benbakoura"
+author = "Mansour Benbakoura"
+release = "0.2.3"
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",  # Pull docstrings
+    "sphinx.ext.autosummary",  # Generate summaries
+    "sphinx.ext.napoleon",  # Support Google/Numpy docstrings
+    "sphinx.ext.viewcode",  # Links to source code,
+    "sphinx_rtd_theme",
+]
+
+autosummary_generate = True  # Automatically generate summaries
+
+# -- Autodoc settings --------------------------------------------------------
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+autodoc_typehints = "description"  # Show type hints in descriptions
+
+# -- Napoleon settings (for Google/Numpy docstrings) -------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+
+# -- HTML output -------------------------------------------------------------
+html_theme = "sphinx_rtd_theme"
+
+
+# -- Auto-generate API docs --------------------------------------------------
+def run_apidoc(_):
+    """Run sphinx-apidoc to auto-generate .rst files."""
+    module_path = Path("../patrick").resolve()
+    output_path = Path("./source").resolve()
+    subprocess.run(
+        ["sphinx-apidoc", "-o", output_path, module_path, "--force"], check=True
+    )
+
+
+def setup(app):
+    app.connect("builder-inited", run_apidoc)
---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. Pattern Discovery Kit documentation master file, created by
+   sphinx-quickstart on Sat Mar 29 17:15:49 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Pattern Discovery Kit documentation
+===================================
+
+Documentation of the ``patrick`` package.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. toctree::
+   :recursive:
+   :caption: Documentation
+
+   modules
+
+.. autosummary::
+   :toctree:
+   :recursive:
+
+   patrick
