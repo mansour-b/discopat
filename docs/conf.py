430045258ddca3538fd4283f5ed129c8a83c7745
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-04-11 15:48:59 +0200

    remove prints in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,9 +5,8 @@ from pathlib import Path
 
 # -- Path setup --------------------------------------------------------------
 on_rtd = os.environ.get("READTHEDOCS") == "True"
-print("on rtd", on_rtd)
 if on_rtd:
-    sys.path.insert(0, os.path.abspath(".."))  # or '../src' if using src layout
+    sys.path.insert(0, Path("..").resolve())
 
 
 # -- Project information -----------------------------------------------------
