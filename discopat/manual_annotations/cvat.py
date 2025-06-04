b871426b4be381929fe01a2e41485b516ac8c462
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-06-04 15:44:52 +0200

    fix padding and rescaling errors

--- discopat/manual_annotations/cvat.py
+++ discopat/manual_annotations/cvat.py
@@ -2,27 +2,56 @@
 from pathlib import Path
 from xml.etree.ElementTree import Element
 
+import numpy as np
 from defusedxml.ElementTree import parse
 
 from discopat.core import Box, Frame, Movie
+from discopat.display import plot_frame
+from discopat.repositories.hdf5 import HDF5Repository
 from discopat.repositories.local import DISCOPATH
 
 # %%
-data_path = DISCOPATH / "annotations" / "250604_115800" / "annotations.xml"
+simulation = "250603_105600"
+annotation_task = "250604_115800"
 
 # %%
-tree = parse(data_path)
+annotation_path = (
+    DISCOPATH / "annotations" / annotation_task / "annotations.xml"
+)
+tree = parse(annotation_path)
 root = tree.getroot()
+
+# %%
+movie_repository = HDF5Repository("tokam2d")
+movie = movie_repository.read(simulation)
+
+num_frames = len(movie.frames)
+width = movie.frames[0].width
+height = movie.frames[0].height
+w = movie.frames[0].image_array.shape[1]
+h = movie.frames[0].image_array.shape[0]
+
+print(num_frames, width, w, height, h)
+
+# %%
+# I messed up:
+# I let VSCode automatically determine the figure size
+# -> I exported 370x370 images
+# Then I did not remove the margins of the saved figure
+# -> savefig added white margins in order to save 640x480 images
+# Now I have to compute these paddings and substract them from the coordinates.
+w_padding = (640 - 370) / 2
+h_padding = (480 - 370) / 2
 
 
 # %%
 def xml_to_box(element: Element) -> Box:
     """Convert CVAT's box annotation to discopat's Box."""
     info_dict = element.attrib
-    xmin = float(info_dict["xtl"])
-    ymin = float(info_dict["ytl"])
-    xmax = float(info_dict["xbr"])
-    ymax = float(info_dict["ybr"])
+    xmin = float(info_dict["xtl"]) - w_padding
+    ymin = float(info_dict["ytl"]) - h_padding
+    xmax = float(info_dict["xbr"]) - w_padding
+    ymax = float(info_dict["ybr"]) - h_padding
 
     return Box(
         label=element.attrib["label"],
@@ -37,8 +66,8 @@ def xml_to_box(element: Element) -> Box:
 def xml_to_frame(element: Element) -> Frame:
     return Frame(
         name=element.attrib["name"].split(".")[0],
-        width=int(element.attrib["width"]),
-        height=int(element.attrib["height"]),
+        width=int(element.attrib["width"]) - 2 * w_padding,
+        height=int(element.attrib["height"]) - 2 * h_padding,
         annotations=[xml_to_box(xml_bbox) for xml_bbox in element],
     )
 
@@ -54,4 +83,23 @@ def xml_to_movie(element: Element) -> Movie:
 
 
 # %%
-movie = xml_to_movie(root)
+image_dict = {frame.name: frame.image_array for frame in movie.frames}
+
+# %%
+annotated_movie = xml_to_movie(root)
+
+# %%
+
+for frame in annotated_movie.frames:
+    print(frame.width, frame.height)
+    frame.resize(target_width=width, target_height=height)
+    print(frame.width, frame.height)
+    frame.image_array = image_dict[frame.name]
+    plot_frame(frame)
+
+# %%
+import matplotlib as mpl
+
+# %%
+print(mpl.rcParams["figure.dpi"])
+# %%
