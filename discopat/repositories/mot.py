6d9cf66c76c7d61dadf9f02c990907c879881a7c
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-07-21 09:57:42 +0200

    wip motrepo

--- discopat/repositories/mot.py
+++ discopat/repositories/mot.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
+from collections import defaultdict
 from pathlib import Path
 
 import numpy as np
 from PIL import Image
 
-from discopat.core import Frame, Movie
+from discopat.core import Box, Frame, Movie
 from discopat.repositories.local import DATA_DIR_PATH, LocalRepository
 
 
@@ -22,20 +25,23 @@ class MOTRepository(LocalRepository):
         )
         data_path = self._input_directory_path / movie_info["set"] / data_folder
         image_path = data_path / "img1"
-        annotation_path = data_path / "det"
+        annotation_path = data_path / "det" / "det.txt"
+
+        annotation_dict = self._load_annotations(annotation_path)
 
         movie = Movie(name=str(content_path), frames=[], tracks=[])
         for path in image_path.glob("*.jpg"):
+            frame_name = path.stem
             image = Image.open(path)
             image_array = np.array(image)
             height, width = image_array.shape[:2]
             movie.frames.append(
                 Frame(
-                    name=path.stem,
+                    name=frame_name,
                     width=width,
                     height=height,
                     image_array=image_array,
-                    annotations=[],
+                    annotations=annotation_dict[int(frame_name)],
                 )
             )
         return movie
@@ -51,6 +57,25 @@ class MOTRepository(LocalRepository):
             "set": dataset,
             "detection_model": detection_model,
         }
+
+    @staticmethod
+    def _load_annotations(annotation_path: Path) -> defaultdict[int, list[Box]]:
+        res = defaultdict()
+        with Path.open(annotation_path) as f:
+            for line in f:
+                print(line)
+                frame_id, _, x, y, w, h, _, _, _ = line.split(",")
+                res[int(frame_id)].append(
+                    Box(
+                        label="pedestrian",
+                        x=float(x),
+                        y=float(y),
+                        width=float(w),
+                        height=float(h),
+                        score=1.0,
+                    )
+                )
+        return res
 
     def write(self, content_path: str or Path, content: Movie) -> None:
         pass
