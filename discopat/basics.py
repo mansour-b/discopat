8ec6537a399c00b951b1cf6a6f613d39bf481b88
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-07-24 13:33:24 +0200

    convert to int8

--- discopat/basics.py
+++ discopat/basics.py
@@ -4,4 +4,4 @@ import numpy as np
 def to_int(array: np.array) -> np.array:
     vmin = array.min()
     vmax = array.max()
-    return ((array - vmin) / (vmax - vmin) * 255).astype(int)
+    return ((array - vmin) / (vmax - vmin) * 255).astype(np.uint8)
