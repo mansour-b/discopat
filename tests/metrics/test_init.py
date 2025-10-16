39563e46ff7f4ce78a43de6fac428a1032cc5688
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-16 17:40:54 +0200

    add tests and ignore useless warnings

--- tests/metrics/test_init.py
+++ tests/metrics/test_init.py
@@ -1,18 +1,35 @@
 import numpy as np
+import pytest
 
 from discopat.metrics import compute_ap_at_threshold
 
 
 class TestMetrics:
-    def test_compute_ap_at_threshold(self):
-        # 2 ground truths
-        groundtruths = [[0, 0, 1, 1]]  # , [2, 2, 3, 3]]
-
-        # 3 predictions
-        predictions = [[0, 0, 1, 1, 0.9]]  # ,  # perfect match with gt[0]
-        #     [2, 2, 3, 3, 0.8],  # perfect match with gt[1]
-        #     [4, 4, 5, 5, 0.7],  # no match
-        # ]
-
+    @pytest.mark.parametrize(
+        ("groundtruths", "predictions", "expected"),
+        [
+            pytest.param(
+                [[0, 0, 1, 1]],
+                [[0, 0, 1, 1, 0.9]],
+                1.0,
+            ),
+            pytest.param(
+                [[0, 0, 1, 1], [2, 2, 3, 3]],
+                [[0, 0, 1, 1, 0.9], [2, 2, 3, 3, 0.8]],
+                1.0,
+            ),
+            pytest.param(
+                [[0, 0, 1, 1], [2, 2, 3, 3]],
+                [[0, 0, 1, 1, 0.9], [2, 2, 3, 3, 0.8], [4, 4, 5, 5, 0.7]],
+                1.0,
+            ),
+            pytest.param(
+                [[0, 0, 1, 1], [2, 2, 3, 3]],
+                [[0, 0, 1, 1, 0.9], [2, 2, 3, 3, 0.6], [4, 4, 5, 5, 0.7]],
+                1.0,
+            ),
+        ],
+    )
+    def test_compute_ap_at_threshold(self, groundtruths, predictions, expected):
         ap50 = compute_ap_at_threshold(groundtruths, predictions, threshold=0.5)
-        assert np.isclose(ap50, 1)
+        assert np.isclose(ap50, expected)
