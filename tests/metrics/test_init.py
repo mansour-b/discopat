b5c6c3c84cb4e011f1820ba309d6cc69c37a47ba
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-16 17:59:09 +0200

    add test

--- tests/metrics/test_init.py
+++ tests/metrics/test_init.py
@@ -14,6 +14,16 @@ class TestMetrics:
                 1.0,
             ),
             pytest.param(
+                [[0, 0, 1, 1]],
+                [[0, 0, 0.5, 1, 0.9]],
+                1.0,
+            ),
+            pytest.param(
+                [[0, 0, 1, 1]],
+                [[0, 0, 0.4, 1, 0.9]],
+                0.0,
+            ),
+            pytest.param(
                 [[0, 0, 1, 1], [2, 2, 3, 3]],
                 [[0, 0, 1, 1, 0.9], [2, 2, 3, 3, 0.8]],
                 1.0,
@@ -30,6 +40,25 @@ class TestMetrics:
             ),
         ],
     )
-    def test_compute_ap_at_threshold(self, groundtruths, predictions, expected):
+    def test_compute_ap50(self, groundtruths, predictions, expected):
         ap50 = compute_ap_at_threshold(groundtruths, predictions, threshold=0.5)
         assert np.isclose(ap50, expected)
+
+    @pytest.mark.parametrize(
+        ("threshold", "expected"),
+        [
+            pytest.param(0.5, 1.0),
+            pytest.param(0.6, 1.0),
+            pytest.param(0.7, 1.0),
+            pytest.param(0.8, 1.0),
+            pytest.param(0.9, 1.0),
+            pytest.param(0.95, 1.0),
+        ],
+    )
+    def test_compute_ap_multiple_thresholds(self, threshold, expected):
+        groundtruths = [[0, 0, 1, 1], [2, 2, 3, 3]]
+        predictions = [[0, 0, 1, 1, 0.9], [2, 2, 3, 3, 0.5]]
+        ap = compute_ap_at_threshold(
+            groundtruths, predictions, threshold=threshold
+        )
+        assert np.isclose(ap, expected)
