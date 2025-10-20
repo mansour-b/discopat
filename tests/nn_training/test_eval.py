0edebd7febe3536d72c881ef6ead8c46f29726de
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-20 16:05:15 +0200

    wip test eval

---
+++ tests/nn_training/test_eval.py
@@ -0,0 +1,34 @@
+from typing import Self
+
+from dsicopat.nn_training.eval import evaluate
+
+from discopat.core import ComputingDevice, Frame, Model
+
+
+class TestEval:
+    @staticmethod
+    def make_model():
+        class DumbModel(Model):
+            def pre_process(self):
+                pass
+
+            def post_process(self):
+                pass
+
+            def predict(self, frame: Frame) -> Frame:
+                return frame
+
+            @classmethod
+            def from_dict(cls, model_as_dict: dict) -> Self:
+                raise NotImplementedError
+
+            def to_dict(self) -> dict:
+                raise NotImplementedError
+
+            def set_device(self, device: ComputingDevice) -> None:
+                raise NotImplementedError
+
+        return DumbModel()
+
+    def test_evaluate(self):
+        pass
