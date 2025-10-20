f4cfba00a992ab8d137427850e156810d62900cd
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-20 16:11:41 +0200

    wip

--- tests/nn_training/test_evaluation.py
+++ tests/nn_training/test_evaluation.py
@@ -1,8 +1,9 @@
 from typing import Self
 
-from dsicopat.nn_training.evaluation import evaluate
+import pytest
 
 from discopat.core import ComputingDevice, Frame, Model
+from discopat.nn_training.evaluation import evaluate, match_gts_and_preds
 
 
 class TestEval:
@@ -29,6 +30,27 @@ class TestEval:
                 raise NotImplementedError
 
         return DumbModel()
+
+    @pytest.mark.parametrize(
+        ("groundtruths", "predictions", "expected"),
+        [
+            pytest.param([[0, 0, 1, 1]], [[0, 0, 1, 1]], 1.0),
+            pytest.param([[0, 0, 1, 1]], [[0.5, 0, 1.5, 1]], 0.5),
+            pytest.param([[0, 0, 1, 1]], [[0.5, 0.5, 1.5, 1.5]], 0.25),
+        ],
+    )
+    def test_match_gts_and_preds(self, groundtruths, predictions, expected):
+        for threshold in [0.5, 0.75, 0.95]:
+            for localization_criterion in ["iou", "iomean"]:
+                assert (
+                    match_gts_and_preds(
+                        groundtruths,
+                        predictions,
+                        threshold,
+                        localization_criterion,
+                    )
+                    == expected
+                )
 
     def test_evaluate(self):
         pass
