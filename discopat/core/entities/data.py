31ef8ba64018996051fe2e2d476f53ac07a901a7
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-23 16:13:17 +0200

    dataset interface

--- discopat/core/entities/data.py
+++ discopat/core/entities/data.py
@@ -14,8 +14,17 @@ X_co = TypeVar("X_co", covariant=True)
 Y_co = TypeVar("Y_co", covariant=True)
 
 
-class Dataset:
-    """Generic interface for datasets."""
+@runtime_checkable
+class Dataset(Protocol[X_co, Y_co]):
+    """A dataset providing indexed access to samples."""
+
+    def __getitem__(self, index: int) -> tuple[X_co, Y_co]:
+        """Return a single sample (X, y) at the given index."""
+        ...
+
+    def __len__(self) -> int:
+        """Return the number of samples in the dataset."""
+        ...
 
 
 @runtime_checkable
@@ -34,6 +43,6 @@ class DataLoader(Protocol[X_co, Y_co]):
         ...
 
     @property
-    def dataset(self) -> Dataset:
+    def dataset(self) -> Dataset[X_co, Y_co]:
         """Return the underlying dataset from which batches are drawn."""
         ...
