6570449ec38ee453127040cabb0a900019770c4b
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-27 17:35:45 +0100

    fix matching tests

--- tests/nn_training/evaluation/test_matching.py
+++ tests/nn_training/evaluation/test_matching.py
@@ -11,62 +11,88 @@ class TestMatching:
         (
             "groundtruths",
             "predictions",
-            "threshold",
             "localization_criterion",
             "expected",
         ),
         [
             pytest.param(
-                [[0, 0, 1, 1]], [[0, 0, 1, 1, 0.9]], 0.5, "iou", (1, (0.9, 1))
+                [[0, 0, 1, 1]],
+                [[0, 0, 1, 1, 0.9]],
+                "iou",
+                {"matching_matrix": np.array([[1]]), "scores": np.array([0.9])},
             ),
             pytest.param(
                 [[0, 0, 1, 1]],
                 [[0.5, 0, 1.5, 1, 0.9]],
-                0.5,
                 "iou",
-                (1, (0.9, 0)),
+                {
+                    "matching_matrix": np.array([[1 / 3]]),
+                    "scores": np.array([0.9]),
+                },
             ),
             pytest.param(
                 [[0, 0, 1, 1]],
                 [[0.5, 0.5, 1.5, 1.5, 0.9]],
-                0.5,
                 "iou",
-                (1, (0.9, 0)),
+                {
+                    "matching_matrix": np.array([[1 / 7]]),
+                    "scores": np.array([0.9]),
+                },
             ),
             pytest.param(
                 [[0, 0, 1, 1]],
                 [[0, 0, 1, 1, 0.9]],
-                0.5,
                 "iomean",
-                (1, (0.9, 1)),
+                {
+                    "matching_matrix": np.array([[1]]),
+                    "scores": np.array([0.9]),
+                },
             ),
             pytest.param(
                 [[0, 0, 1, 1]],
                 [[0.5, 0, 1.5, 1, 0.9]],
-                0.5,
                 "iomean",
-                (1, (0.9, 1)),
+                {
+                    "matching_matrix": np.array([[0.5]]),
+                    "scores": np.array([0.9]),
+                },
             ),
             pytest.param(
                 [[0, 0, 1, 1]],
                 [[0.5, 0.5, 1.5, 1.5, 0.9]],
-                0.5,
                 "iomean",
-                (1, (0.9, 0)),
+                {
+                    "matching_matrix": np.array([[1 / 4]]),
+                    "scores": np.array([0.9]),
+                },
             ),
             pytest.param(
                 [[0, 0, 1, 1], [2, 2, 3, 3]],
                 [[0, 0, 1, 1, 0.5], [2, 2, 3, 3, 0.9]],
-                0.5,
                 "iou",
-                (2, [(0.9, 1), (0.5, 1)]),
+                {
+                    "matching_matrix": np.array(
+                        [
+                            [0, 1],
+                            [1, 0],
+                        ]
+                    ),
+                    "scores": np.array([0.9, 0.5]),
+                },
             ),
             pytest.param(
                 [[0, 0, 1, 1], [2, 2, 3, 3]],
                 [[3, 3, 4, 4, 0.9], [2, 2, 3, 3, 0.5]],
-                0.5,
                 "iou",
-                (2, [(0.9, 0), (0.5, 1)]),
+                {
+                    "matching_matrix": np.array(
+                        [
+                            [0, 0],
+                            [0, 1],
+                        ]
+                    ),
+                    "scores": np.array([0.9, 0.5]),
+                },
             ),
         ],
     )
@@ -74,18 +100,16 @@ class TestMatching:
         self,
         groundtruths,
         predictions,
-        threshold,
         localization_criterion,
         expected,
     ):
         boxes = [p[:4] for p in predictions]
         scores = [p[-1] for p in predictions]
-        num_groundtruths, tp_vector = match_groundtruths_and_predictions(
+        res = match_groundtruths_and_predictions(
             groundtruths,
             boxes,
             scores,
-            threshold,
             localization_criterion,
         )
-        assert num_groundtruths == expected[0]
-        assert np.allclose(tp_vector, expected[1])
+        assert np.allclose(res["matching_matrix"], expected["matching_matrix"])
+        assert np.allclose(res["scores"], expected["scores"])
