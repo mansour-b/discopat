dbaa3ae5585ebea3866977ea05e693d556b6e9e0
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-11-03 15:50:17 +0100

    add switch to disable mps

--- discopat/utils.py
+++ discopat/utils.py
@@ -1,9 +1,11 @@
 import torch
 
 
-def get_device():
+def get_device(allow_mps: bool = False):
     if torch.cuda.is_available():
         return "cuda"
+    if not allow_mps:
+        return "cpu"
     if torch.mps.is_available():
         return "mps"
     return "cpu"
