148fd35d507a3b9358e7ef8f35701746e75e7415
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-11-05 18:21:30 +0100

    add test for to int + fix typos

--- tests/test_display.py
+++ tests/test_display.py
@@ -9,6 +9,7 @@ from discopat.display import frame_to_pil, get_center_path, to_int
     ("image_array", "expected"),
     [
         pytest.param([0, 0, 1, 1], [0, 0, 255, 255]),
+        pytest.param([0, 0, 0, 0], [0, 0, 0, 0]),
         pytest.param([0.0, 0.0, 1.0, 1.0], [0, 0, 255, 255]),
         pytest.param([-1, 1, -1, 1], [0, 255, 0, 255]),
     ],
@@ -18,7 +19,7 @@ def test_to_int(image_array, expected):
 
 
 def test_get_center_path():
-    track_ids = np.arange(10).reshaoe(-1, 1)
+    track_ids = np.arange(10).reshape(-1, 1)
     xs = np.arange(10).reshape(-1, 1)
     ys = np.arange(0, 20, 2).reshape(-1, 1)
 
@@ -37,7 +38,7 @@ def test_frame_to_pil():
         image_array=np.zeros((10, 10)),
     )
     tracks = {
-        0: np.hstack([np.arange(10).reshape(-1, 1), np.zeros(10, 4)]),
+        0: np.hstack([np.arange(10).reshape(-1, 1), np.zeros((10, 4))]),
         1: np.hstack(
             [
                 np.arange(200),
