72037d244552202c82197dab0f316038bfa53850
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-11-05 18:52:36 +0100

    finish test of frame_to_pil

--- tests/test_display.py
+++ tests/test_display.py
@@ -1,3 +1,5 @@
+import hashlib
+
 import numpy as np
 import pytest
 
@@ -30,26 +32,37 @@ def test_get_center_path():
 
 
 def test_frame_to_pil():
+    frame_id = 100
     frame = Frame(
-        name="100",
-        width=10,
-        height=10,
+        name=str(frame_id),
+        width=100,
+        height=100,
         annotations=[],
-        image_array=np.zeros((10, 10)),
+        image_array=np.zeros((100, 100)),
     )
     tracks = {
         0: np.hstack([np.arange(10).reshape(-1, 1), np.zeros((10, 4))]),
         1: np.hstack(
             [
                 np.arange(200).reshape(-1, 1),
-                np.ones((200, 1)),
-                np.ones((200, 1)),
-                np.ones((200, 1)),
-                np.ones((200, 1)),
+                np.linspace(10, 80, 200).reshape(-1, 1),
+                np.linspace(5, 50, 200).reshape(-1, 1),
+                np.linspace(20, 85, 200).reshape(-1, 1),
+                np.linspace(20, 70, 200).reshape(-1, 1),
             ]
         ),
     }
+    x = tracks[1][frame_id, 1]
+    y = tracks[1][frame_id, 2]
+    width = tracks[1][frame_id, 3] - x
+    height = tracks[1][frame_id, 4] - y
+    frame.annotations = [
+        Box(label="dummy", x=x, y=y, width=width, height=height, score=1.0)
+    ]
 
     pil_image = frame_to_pil(frame, tracks)
-
-    assert np.allclose(np.array(pil_image), np.ones((10, 10, 3)))
+    expected_hash = (
+        "35e1e20a41d69c1f5724d5ef1c1e8339236c31d30818042d87330a2aefc39613"
+    )
+    actual_hash = hashlib.sha256(np.array(pil_image)).hexdigest()
+    assert actual_hash == expected_hash
