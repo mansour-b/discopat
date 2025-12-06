33d54f797de5e34545267ac07eb156f9c8bb3e50
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-12-06 02:14:13 +0100

    remove copyright comments

--- discopat/nn_training/detr/criterion.py
+++ discopat/nn_training/detr/criterion.py
@@ -1,4 +1,3 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """DETR criterion class."""
 
 import torch
--- discopat/nn_training/detr/matcher.py
+++ discopat/nn_training/detr/matcher.py
@@ -1,4 +1,3 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """Compute the matching cost and solve the corresponding LSAP."""
 
 import torch
